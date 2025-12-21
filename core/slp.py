796f188bf1a4aa659b81fb1a412c42fe629fca1a
abdelkrimtabti <aimaneabdelkrimtabti@gmail.com>
2025-12-21 18:31:19 +0100

    update slp.py

--- core/slp.py
+++ core/slp.py
@@ -1,1 +1,52 @@
+import pandas as pd
+import math
+
+
+
+def convert_adjacency_matrix(adj_matrix):
+    values = {
+        "A": 6,
+        "E": 5,
+        "I": 4,
+        "O": 3,
+        "U": 2,
+        "X": 0
+    }
+    return adj_matrix.replace(values)
+
+
+
+
+def generate_slp_layout(departments):
+    layout = {}
+
+    
+    cols = math.ceil(math.sqrt(len(departments)))
+
+    for i, dept in enumerate(departments):
+        x = i % cols      
+        y = i // cols
+        layout[dept] = (x, y)
+
+    return layout
+
+
+
+
+def rectilinear_distance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+
+def slp_total_distance(flow_matrix, layout):
+    total = 0
+
+    for i in flow_matrix.index:
+        for j in flow_matrix.columns:
+            if i != j:
+                fij = flow_matrix.loc[i, j]
+                dij = rectilinear_distance(layout[i], layout[j])
+                total += fij * dij
+
+    return total
 
